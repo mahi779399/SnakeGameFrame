7c6f4d10c98667d7affd19b7dde3e52c0d0671a0
mahi779399 <mahi779399@gmail.com>
2024-07-07 20:43:53 +0530

    readme file modified with content

--- StartGame.java
+++ StartGame.java
@@ -13,14 +13,14 @@ class StartGame extends JFrame {
     public StartGame() {
         setTitle("Snake Game");
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        gamePanel2 = new GamePanel2();
+        gamePanel2 = new GamePanel2(MainMenuActions.levelSelected);
         add(gamePanel2);
         pack();
         setLocationRelativeTo(null);
         setResizable(false);
         SnakeGameMainMenu.frameIcon(this);
         setVisible(true);
-        gameFrame=this;
+        gameFrame = this;
     }
 }
 
@@ -40,22 +40,23 @@ class GamePanel2 extends JPanel implements ActionListener {
     private Timer timer;
     private final Random random;
     static int highestScore = 0;
-    Color initialColor = Color.RED; 
-    int levelSelected = 0; 
+    Color initialColor = MainMenuActions.initialColor;
 
-    public GamePanel2() {
+    public GamePanel2(int levelSelected) {
         random = new Random();
         this.setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
-        this.setBackground(Color.BLACK); 
+        this.setBackground(Color.BLACK);
         this.setFocusable(true);
         this.addKeyListener(new GameKeyAdapter());
         setSpeed(levelSelected);
         startGame();
     }
+
     public void updateInitialColor(Color newColor) {
         initialColor = newColor;
-        repaint(); 
+        repaint();
     }
+
     public void startGame() {
         snakeLength = 3;
         for (int i = 0; i < snakeLength; i++) {
@@ -71,17 +72,17 @@ class GamePanel2 extends JPanel implements ActionListener {
     private void setSpeed(int levelSelected) {
         switch (levelSelected) {
             case 0:
-                DELAY = 100; 
+                DELAY = 100;
                 break;
             case 50:
-                DELAY = 50; 
+                DELAY = 50;
                 break;
             case 100:
-                DELAY = 20; 
+                DELAY = 20;
                 break;
         }
     }
-    
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -90,19 +91,19 @@ class GamePanel2 extends JPanel implements ActionListener {
 
     public void draw(Graphics g) {
         if (running) {
-            g.setColor(Color.WHITE); 
+            g.setColor(Color.WHITE);
             g.fillOval(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
 
             for (int i = 0; i < snakeLength; i++) {
                 if (i == 0) {
-                    g.setColor(initialColor); 
+                    g.setColor(initialColor);
                 } else {
                     g.setColor(initialColor);
                 }
                 g.fillRect(snakeX[i], snakeY[i], UNIT_SIZE, UNIT_SIZE);
             }
 
-            g.setColor(initialColor); 
+            g.setColor(initialColor);
             g.setFont(new Font("Arial", Font.BOLD, 20));
             FontMetrics metrics = getFontMetrics(g.getFont());
             g.drawString("Score: " + (snakeLength - 1), (BOARD_WIDTH - metrics.stringWidth("Score: " + (snakeLength - 1))) / 2, g.getFont().getSize());
@@ -175,28 +176,28 @@ class GamePanel2 extends JPanel implements ActionListener {
         gameOverFrame.setResizable(false);
         gameOverFrame.setLayout(new BorderLayout());
         gameOverFrame.setSize(400, 400);
-        gameOverFrame.getContentPane().setBackground(Color.BLACK); 
+        gameOverFrame.getContentPane().setBackground(Color.BLACK);
 
         JPanel centerPanel = new JPanel(new GridLayout(3, 1));
-        centerPanel.setBackground(Color.BLACK); 
+        centerPanel.setBackground(Color.BLACK);
 
         JLabel gameOverLabel = new JLabel("Game Over", SwingConstants.CENTER);
         gameOverLabel.setFont(new Font("Arial", Font.BOLD, 20));
-        gameOverLabel.setForeground(initialColor); 
+        gameOverLabel.setForeground(initialColor);
         centerPanel.add(gameOverLabel);
 
         JLabel scoreLabel = new JLabel("Your score: " + (snakeLength - 1));
-        scoreLabel.setForeground(initialColor); 
+        scoreLabel.setForeground(initialColor);
         centerPanel.add(scoreLabel);
 
         JLabel highestScoreLabel = new JLabel("Highest score: " + highestScore);
-        highestScoreLabel.setForeground(initialColor); 
+        highestScoreLabel.setForeground(initialColor);
         centerPanel.add(highestScoreLabel);
 
         gameOverFrame.add(centerPanel, BorderLayout.CENTER);
 
         JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
-        buttonPanel.setBackground(Color.BLACK); 
+        buttonPanel.setBackground(Color.BLACK);
 
         JButton quitGameButton = new JButton("Quit Game");
         quitGameButton.addActionListener(new ActionListener() {
@@ -206,8 +207,8 @@ class GamePanel2 extends JPanel implements ActionListener {
                 System.exit(0);
             }
         });
-        quitGameButton.setForeground(Color.BLACK); 
-        quitGameButton.setBackground(initialColor); 
+        quitGameButton.setForeground(Color.BLACK);
+        quitGameButton.setBackground(initialColor);
         buttonPanel.add(quitGameButton);
 
         JButton mainMenuButton = new JButton("Main Menu");
@@ -219,8 +220,8 @@ class GamePanel2 extends JPanel implements ActionListener {
                 StartGame.gameFrame.dispose();
             }
         });
-        mainMenuButton.setForeground(Color.BLACK); 
-        mainMenuButton.setBackground(initialColor); 
+        mainMenuButton.setForeground(Color.BLACK);
+        mainMenuButton.setBackground(initialColor);
         buttonPanel.add(mainMenuButton);
 
         gameOverFrame.add(buttonPanel, BorderLayout.SOUTH);
@@ -246,25 +247,25 @@ class GamePanel2 extends JPanel implements ActionListener {
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_A:
                 case KeyEvent.VK_LEFT:
-                    if (direction!= 'R') {
+                    if (direction != 'R') {
                         direction = 'L';
                     }
                     break;
                 case KeyEvent.VK_D:
                 case KeyEvent.VK_RIGHT:
-                    if (direction!= 'L') {
+                    if (direction != 'L') {
                         direction = 'R';
                     }
                     break;
                 case KeyEvent.VK_W:
                 case KeyEvent.VK_UP:
-                    if (direction!= 'D') {
+                    if (direction != 'D') {
                         direction = 'U';
                     }
                     break;
                 case KeyEvent.VK_S:
                 case KeyEvent.VK_DOWN:
-                    if (direction!= 'U') {
+                    if (direction != 'U') {
                         direction = 'D';
                     }
                     break;
